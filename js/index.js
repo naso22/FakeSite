3b4683909abba3d65596f142f58ad08d21471c4b
柴田成clo <shibata.naru@cloco.co.jp>
2024-02-05 18:32:43 +0900

    commit

--- js/index.js
+++ js/index.js
@@ -3,15 +3,6 @@ $(function () {
 		$('.nav').toggleClass('open');  // メニューにopenクラスをつけ外しする
 	});
 
-$('.slide-items').slick({
-	dots:true,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  adaptiveHeight: true,
-  arrows:true ,
-});
-
 $(window).on("scroll", function () {
   // ウィンドウのスクロール位置を取得
   const scrollTop = $(this).scrollTop();
@@ -27,4 +18,29 @@ $(window).on("scroll", function () {
 });
 
 
+});
+
+const swiper = new Swiper(".swiper", {
+  autoplay: {
+    delay: 4000,
+  },
+  speed: 500,
+  loop: true,
+  effect: "fade",
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
 });
